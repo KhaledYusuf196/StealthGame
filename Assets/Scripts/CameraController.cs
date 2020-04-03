4e8f4bb4d1a53ee48144b11fbd23b05d248e9189
Khaled Yousef <khaledyousef196@gmail.com>
2020-04-03 17:04:20 +0200

    code and prefabs polish

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Transform CameraTransform;
+    Vector3 Offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraTransform = Camera.main.transform;
+        Offset = CameraTransform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraTransform.position = transform.position + Offset;
+        CameraTransform.LookAt(transform.position);
+    }
+}
